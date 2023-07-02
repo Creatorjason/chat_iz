d97d1e9f4fec54ea11efbac4fe340180ffdca170
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-07-02 04:20:25 +0100

    feat: updated scripts field run to start and install neccesary dependencies

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+
+
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server is listening and running on port: ${PORT}`))
