f770934badb6aaf440378b5b476260e9dd6bea72
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-07-04 12:21:41 +0100

    feat: created userModel with mongoose

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+
+const userModel = mongoose.Schema(
+    name:{
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    picture : {
+        type: String,
+        required: true, 
+        default: "https://www.flaticon.com/free-icons/hacker" 
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model("User", userModel)
+module.exports = User
